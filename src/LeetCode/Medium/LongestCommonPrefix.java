d895a26c8a1c0fe114eea15c76f3fc9a8fae322a
gunt <0408gun@gmail.com>
2021-01-29 21:47:36 +0900

    알고리즘 문제풀이(Leetcode 문제풀이)
    https://leetcode.com/problems/longest-common-prefix/

---
+++ src/LeetCode/Medium/LongestCommonPrefix.java
@@ -0,0 +1,21 @@
+package LeetCode.Medium;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        System.out.println(longestCommonPrefix(new String[]{"dog","racecar","car"}));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        int length = strs.length;
+        String result = strs[0];
+        for (int i = 1; i < length; i++) {
+            for (int j = result.length(); j >= 0; j--) {
+                result = result.substring(0, j);
+                if (strs[i].startsWith(result)) {
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
