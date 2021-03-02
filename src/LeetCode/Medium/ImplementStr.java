d069ee04ccadb11f71b8d81d7277c831c5027aa1
gunt <0408gun@gmail.com>
2021-03-02 20:12:51 +0900

    알고리즘 문제풀이(leetcode 문제풀이)

---
+++ src/LeetCode/Medium/ImplementStr.java
@@ -0,0 +1,8 @@
+package LeetCode.Medium;
+
+public class ImplementStr {
+    public int strStr(String haystack, String needle) {
+        if (!haystack.contains(needle)) return -1;
+        return haystack.indexOf(needle);
+    }
+}
