043a2e4f153f973abcb33a12a4328b25a9f36c05
gunt <0408gun@gmail.com>
2021-03-19 18:32:31 +0900

    알고리즘 문제풀이

--- src/Programmers/Basic/Num43163.java
+++ src/Programmers/Basic/Num43163.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.Stack;
 
 public class Num43163 {
+    public static void main(String[] args) {
+        System.out.println("3".charAt(0)-'0');
+    }
     public int solution(String begin, String target, String[] words) {
         if (!Arrays.asList(words).contains(target)) return 0;
         int cnt = 0;
@@ -19,12 +22,19 @@ public class Num43163 {
                     if (diff > 1) break;
                 }
                 if (diff == 1) {
-                    if (word.equals(target)) return cnt;
+                    if (word.equals(target)) return cnt + 1;
                     else begins.add(word);
                 }
             }
         }
         return 0;
     }
-
+    private int sum(int isEven, String cardNum){
+        int sum = 0;
+        for(int i = cardNum.length() - 1 - isEven; i > 0; i= i - 2 + isEven){
+            int temp = (cardNum.charAt(i) - '0') * (1 + isEven);
+            sum += (temp/10) + (temp%10);
+        }
+        return sum;
+    }
 }
